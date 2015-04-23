fc5e5bc5a724fca0107c28cd0396e69cbb5459e8
miniesta4 <mgnstg@gmail.com>
2015-04-23 14:24:22 +0200

    v01

--- Repro/stormResearch.R
+++ Repro/stormResearch.R
@@ -1,21 +1,32 @@
-StormResearch <- function(){}
+stormResearch <- function(){
 
-## Get file
-
+## Download file
 if !file.exists ("./files/StormData.bz2"){
     fileUrl <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
     download.file(fileUrl, destfile = "./files/StormData.bz2", method = "wget")
+    dateDownloaded <- date()  # Date downloaded
 }
 
-dateDownloaded <- date()
-
-if (!data) {
-    data5r <- read.csv("./files/StormData.bz2", stringsAsFactors = FALSE, na.strings = "", nrow = 5)
-    classes <- sapply(data5r, class)
-    data <- read.csv("./files/StormData.bz2", stringsAsFactors = FALSE, na.strings = "", colClasses = classes)
+## Read file
+if (!ds) {
+    
+    ##  data5r <- read.csv("./files/StormData.bz2", stringsAsFactors = FALSE,
+    na.strings = "", nrow = 5)
+##  classes <- sapply(data5r, class)
+ds <- read.csv("./files/StormData.bz2", stringsAsFactors = FALSE, 
+               na.strings = "")
 }
-dim(data)
-summary(data)
-names(data)
-sapply(data, function(x) mean(is.na(x)))
 
+## Data summary
+dim(ds)
+summary(ds)
+names(ds)
+sort(round(sapply(ds, function(x) mean(is.na(x))), digits = 2), decreasing = TRUE)
+
+head(ds$EVTYPE)
+ag <- aggregate(cbind(FATALITIES, INJURIES) ~ EVTYPE, ds = ds, sum)
+ag[order(ag$FATALITIES, ag$INJURIES, decreasing = TRUE), ]
+
+ag[ag$FATALITIES > 0 | ag$INJURIES > 0, ]
+
+}
